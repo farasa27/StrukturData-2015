23b862f22502e38bff250b9af9befef9e4eda379
Farrasa Rani Faisyal <farasarani@gmail.com>
2015-10-30 17:29:16 +0700

    no message

--- vector 1/Vector3D.java
+++ vector 1/Vector3D.java
@@ -8,20 +8,39 @@
 public class Vector3D
 {
     // instance variables - replace the example below with your own
-    public double x;
-    public double y; 
-    public double z;
+    private double x;
+    private double y; 
+    private double z;
 
     /**
      * Constructor for objects of class Vector3D
      */
-    public Vector3D(double x,double y, double z)
-    {
-        // initialise instance variables
-        this.x = x; 
-        this.y = y; 
-        this.z = z;
+     public Vector3D(double x,double y, double z)
+      {
+    this.x =x; 
+    this.y =y; 
+     this.z=z; 
+    
     } 
+     
+     
+     public double getX()
+    { 
+        
+        
+    
+        return x;  
+    }
+        public double getY()
+    { 
+        return y; 
+    }
+        public double getZ()
+    { 
+        return z; 
+    }
+        // initialise instance variables
+       
 }
 
    
