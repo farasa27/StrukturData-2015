6128af77468398c2b69f466976f31441c3af682b
Farrasa Rani Faisyal <farasarani@gmail.com>
2015-11-05 20:18:53 +0700

    no message

---
+++ -Kebun-Binatang/Mamalia.java
@@ -0,0 +1,6 @@
+    public interface Mamalia extends Binatang {
+    public int jumlahKaki();
+ 
+
+
+}
---
+++ -Kebun-Binatang/Ular.java
@@ -0,0 +1,11 @@
+   public class Ular implements Reptilia   {
+      public String suara(){
+        return "Sss";
+    
+     } 
+    
+    
+       public boolean merayap()  {
+        return true;
+     }
+}
