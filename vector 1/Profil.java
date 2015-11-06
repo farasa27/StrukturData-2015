4af088cdd63bead33f53104e544694bb6c4cc183
Farrasa Rani Faisyal <farasarani@gmail.com>
2015-11-06 14:46:11 +0700

    no message

---
+++ vector 1/Profil.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Profil here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Profil
+{
+    // instance variables - replace the example below with your own
+    private String nama="Farrasa Rani Faisyal"; 
+    private String nim="1408107010034";
+
+    
+    public Profil ()
+    {
+       
+    }
+
+    public Profil (String nama,String nim) 
+    { 
+        this.nama=nama;
+        this.nim=nim;
+        
+    } 
+    public String getNama() 
+    { 
+        return nama; 
+    }  
+    public String getNim() 
+    { 
+        return nim; 
+    }  
+    
+}
