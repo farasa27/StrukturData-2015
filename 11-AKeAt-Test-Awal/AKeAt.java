ea6010b41a2923d2cd2c1c23e31ad165dfd3df96
Farrasa Rani Faisyal <farasarani@gmail.com>
2015-11-20 15:10:33 +0700

    no message

---
+++ 11-AKeAt-Test-Awal/AKeAt.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class AKeAt here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+    // instance variables - replace the example below with your own
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AKeAt {
+    public void aKeAt(String sumber, String sasaran) throws IOException{
+              FileInputStream masukan = null;
+               FileOutputStream keluaran = null;
+        
+        try{
+                masukan = new FileInputStream(sumber);
+               keluaran = new FileOutputStream(sasaran);
+            
+                 int karakter = masukan.read();
+            while (karakter != -1){
+                    if(karakter==65 || karakter==97){
+                    karakter=64;
+                }
+                    keluaran.write(karakter);
+                   karakter = masukan.read();
+            }
+            keluaran.flush();
+        }
+        
+           catch (IOException kesalahan) {
+            System.out.printf("Terjadi kesalahan: %s", kesalahan);
+        }
+        
+        finally {
+               if (masukan != null)
+                masukan.close();
+               if (keluaran != null)
+                keluaran.close();
+        }
+    }
+}
