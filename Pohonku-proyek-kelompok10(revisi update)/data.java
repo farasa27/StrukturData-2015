0d3e1a423a26fb47447fc056b50139b83c46c119
Farrasa Rani Faisyal <farasarani@gmail.com>
2016-01-12 15:20:40 +0700

    proyek kelompok 10 revisi update

---
+++ Pohonku-proyek-kelompok10(revisi update)/data.java
@@ -0,0 +1,12 @@
+/**
+ * Write a description of class pohon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class data
+{
+    public String nama, waktu;
+    public String lembab, uv, nitro, suhu;
+    
+}
